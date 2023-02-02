a21cb166dcee327d59f79264270b4f5c3c7631f0
AgusjkDev <agusjkdev7@gmail.com>
2023-02-02 20:02:56 -0300

    Add header to layout

--- packages/website/app/layout.tsx
+++ packages/website/app/layout.tsx
@@ -1,3 +1,5 @@
+import Link from "next/link";
+
 import "./globals.css";
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
@@ -5,7 +7,17 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
         <html lang="es">
             <head />
 
-            <body>{children}</body>
+            <body className="bg-primary-lightest text-primary">
+                <header className="sticky top-0 w-full bg-white p-5 shadow-md">
+                    <Link href="/">
+                        <h1 className="text-center text-2xl font-black text-secondary md:text-3xl">
+                            Hackathon Chat Feedback
+                        </h1>
+                    </Link>
+                </header>
+
+                <div className="px-4 md:px-0">{children}</div>
+            </body>
         </html>
     );
 }
