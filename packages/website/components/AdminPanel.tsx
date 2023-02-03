4b78337df925b741287bb070edfb746b53e92cf7
AgusjkDev <agusjkdev7@gmail.com>
2023-02-03 20:26:10 -0300

    Change increased alert z-index due to header

--- packages/website/components/AdminPanel.tsx
+++ packages/website/components/AdminPanel.tsx
@@ -40,7 +40,7 @@ export default function AdminPanel() {
 
             {showAlert && (
                 <span
-                    className={`fixed top-0 left-0 w-full p-4 text-center font-bold text-white ${
+                    className={`fixed top-0 left-0 z-[2] w-full p-4 text-center font-bold text-white ${
                         alert.type === "success" ? "bg-secondary" : "bg-red-600"
                     }`}
                 >
