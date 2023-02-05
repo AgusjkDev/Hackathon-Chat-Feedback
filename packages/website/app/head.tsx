b90a43fd22b7c266916c44841da984d3f13607cd
AgusjkDev <agusjkdev7@gmail.com>
2023-02-05 00:22:55 -0300

    Add SEO tags and other information files

--- packages/website/app/head.tsx
+++ packages/website/app/head.tsx
@@ -1,8 +1,35 @@
 export default function Head() {
     return (
         <>
+            <meta charSet="utf-8" />
+            <meta name="viewport" content="width=device-width" />
+            <meta name="author" content="Agustín Arnoldi" />
+            <meta
+                name="description"
+                content="Analizaremos el comportamiento del chat mientras midudev esté observando los proyectos de la hackathon de co:here."
+            />
+            <meta name="robots" content="index, follow" />
             <title>Hackathon Chat Feedback</title>
-            <meta content="width=device-width, initial-scale=1" name="viewport" />
+            <meta property="og:title" content="Hackathon Chat Feedback" />
+            <meta property="og:site_name" content="Hackathon Chat Feedback" />
+            <meta property="og:url" content="https://hackathon-chat-feedback.vercel.app" />
+            <meta
+                property="og:description"
+                content="Analizaremos el comportamiento del chat mientras midudev esté observando los proyectos de la hackathon de co:here."
+            />
+            <meta property="og:type" content="website" />
+            <meta
+                property="og:image"
+                content="https://github.com/agusjkdev/hackathon-chat-feedback/blob/main/.github/preview.png"
+            />
+            <link rel="icon" href="/favicons/favicon.ico" sizes="any" />
+            <link rel="icon" type="image/png" sizes="32x32" href="/favicons/favicon-32x32.png" />
+            <link rel="icon" type="image/png" sizes="16x16" href="/favicons/favicon-16x16.png" />
+            <link rel="apple-touch-icon" sizes="180x180" href="/favicons/apple-touch-icon.png" />
+            <link rel="mask-icon" href="/favicons/favicon.svg" color="#5bbad5" />
+            <link rel="manifest" href="/site.webmanifest" />
+            <meta name="msapplication-TileColor" content="#ffffff" />
+            <meta name="theme-color" content="#ffffff" />
         </>
     );
 }
