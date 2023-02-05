eb8c52ea1a5d01770fd50ebe7146340eaf44d551
AgusjkDev <agusjkdev7@gmail.com>
2023-02-05 11:35:58 -0300

    Revert "Change created components"
    This reverts commit 4ed556c1dd73276e1b94a93ab492f941486fc0a2.

--- packages/website/components/LandingText.tsx
+++ packages/website/components/LandingText.tsx
@@ -1,18 +1,37 @@
-import Anchor from "./Anchor";
-
 export default function LandingText() {
     return process.env.NEXT_PUBLIC_HACKATHON_ENDED !== "true" ? (
         <p className="max-w-lg text-center font-medium">
             Analizaremos el comportamiento del chat mientras&nbsp;
-            <Anchor href="https://www.twitch.tv/midudev">midudev</Anchor>
+            <a
+                href="https://www.twitch.tv/midudev"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="text-secondary underline transition-colors duration-300 hover:text-secondary-dark"
+            >
+                midudev
+            </a>
             &nbsp;esté en directo observando los proyectos de la hackathon de&nbsp;
-            <Anchor href="https://docs.cohere.ai">co:here</Anchor>
+            <a
+                href="https://docs.cohere.ai"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="text-secondary underline transition-colors duration-300 hover:text-secondary-dark"
+            >
+                co:here
+            </a>
             &#46;
         </p>
     ) : (
         <p className="max-w-lg text-center font-medium">
             La hackathon acabó, y el proyecto también. Gracias a&nbsp;
-            <Anchor href="https://www.twitch.tv/midudev">midudev</Anchor>
+            <a
+                href="https://www.twitch.tv/midudev"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="text-secondary underline transition-colors duration-300 hover:text-secondary-dark"
+            >
+                midudev
+            </a>
             &nbsp;por la organización y por su servicio a la comunidad de programación💯
         </p>
     );
--- packages/website/components/index.ts
+++ packages/website/components/index.ts
@@ -1,6 +1,4 @@
-export { default as Header } from "./Header";
 export { default as LandingText } from "./LandingText";
 export { default as Projects } from "./Projects";
 export { default as AdminPanel } from "./AdminPanel";
 export { default as LoginForm } from "./LoginForm";
-export { default as Footer } from "./Footer";
