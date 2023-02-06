361a0364fc095bbed7bb71ae48bf2d1128f61065
AgusjkDev <agusjkdev7@gmail.com>
2023-02-06 18:13:58 -0300

    Fix heading anchor size

--- packages/website/components/Header.tsx
+++ packages/website/components/Header.tsx
@@ -2,8 +2,8 @@ import Link from "next/link";
 
 export default function Header() {
     return (
-        <header className="sticky top-0 z-[1] w-full bg-white p-5 shadow-md">
-            <Link href="/">
+        <header className="sticky top-0 z-[1] flex w-full p-5 shadow-md">
+            <Link href="/" className="mx-auto">
                 <h1 className="text-center text-2xl font-black text-secondary md:text-3xl">
                     Hackathon Chat Feedback
                 </h1>
