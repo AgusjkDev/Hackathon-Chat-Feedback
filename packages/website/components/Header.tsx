aef60261a983a23270c342dfaaf279a648e7cf4f
AgusjkDev <agusjkdev7@gmail.com>
2023-02-07 14:21:24 -0300

    Fix header background

--- packages/website/components/Header.tsx
+++ packages/website/components/Header.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 export default function Header() {
     return (
-        <header className="sticky top-0 z-[1] flex w-full p-5 shadow-md">
+        <header className="sticky top-0 z-[1] flex w-full bg-white p-5 shadow-md">
             <Link href="/" className="mx-auto">
                 <h1 className="text-center text-2xl font-black text-secondary md:text-3xl">
                     Hackathon Chat Feedback
